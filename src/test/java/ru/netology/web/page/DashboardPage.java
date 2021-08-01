90860fd765fc061a903f4e62c2e831d5bb614649
Alexander Mizyukin <a.mizyukin@mail.ru>
2021-08-01 22:20:26 +0300

    code and test added

--- src/test/java/ru/netology/web/page/DashboardPage.java
+++ src/test/java/ru/netology/web/page/DashboardPage.java
@@ -1,14 +1,47 @@
 package ru.netology.web.page;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
+import lombok.val;
 
-import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
 
 public class DashboardPage {
     private SelenideElement heading = $("[data-test-id=dashboard]");
+    private static final ElementsCollection cards = $$(".list__item");
+    private static final String balanceStart = "баланс: ";
+    private static final String balanceFinish = " р.";
 
     public DashboardPage() {
         heading.shouldBe(visible);
+    }
+
+    public static int getCardBalance(int index) {
+        val text = cards.get(index).text();
+        return extractBalance(text);
+    }
+
+    private static int extractBalance(String text) {
+        val start = text.indexOf(balanceStart);
+        val finish = text.indexOf(balanceFinish);
+        val value = text.substring(start + balanceStart.length(), finish);
+        return Integer.parseInt(value);
+    }
+
+    public static void changeFirstCardBalanse(String sum) {
+        $("[class=button__content]").click();
+        $("[class=input__control]").setValue(sum);
+        $("[type=tel]").setValue("5559 0000 0000 0002");
+        $("[class=button__text]").click();
+    }
+
+    public static void changeSecondCardBalanse(String sum) {
+        $$("[class=button__content]").get(1).click();
+        $("[class=input__control]").setValue(sum);
+        $("[type=tel]").setValue("5559 0000 0000 0001");
+        $("[class=button__text]").click();
     }
 }
--- src/test/java/ru/netology/web/test/MoneyTransferTest.java
+++ src/test/java/ru/netology/web/test/MoneyTransferTest.java
@@ -1,42 +1,59 @@
 package ru.netology.web.test;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.netology.web.data.DataHelper;
+import ru.netology.web.page.DashboardPage;
 import ru.netology.web.page.LoginPageV1;
 import ru.netology.web.page.LoginPageV2;
 import ru.netology.web.page.LoginPageV3;
 
+import java.time.Duration;
+
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MoneyTransferTest {
-    @Test
-    void shouldTransferMoneyBetweenOwnCardsV1() {
+
+    @BeforeEach
+    public void setUp() {
         open("http://localhost:9999");
         var loginPage = new LoginPageV1();
-//    var loginPage = open("http://localhost:9999", LoginPageV1.class);
         var authInfo = DataHelper.getAuthInfo();
         var verificationPage = loginPage.validLogin(authInfo);
         var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
         verificationPage.validVerify(verificationCode);
     }
 
+//    @Test
+//    void shouldTransferMoneyBetweenOwnCards() {
+//        $("[data-test-id=\"0f3f5c2a-249e-4c3d-8287-09f7a039391d\"] > [data-test-id=\"action-deposit\"").click();
+//        $("[data-test-id=amount] input").setValue("5000");
+//        $("[data-test-id=from] input").setValue("92df3f1c-a033-48e6-8390-206f6b1f56c0\\");
+////        $("[data-test-id=to]").setValue("92df3f1c-a033-48e6-8390-206f6b1f56c0");
+//        $("[data-test-id=\"action-transfer\"]").click();
+//
+//    }
+
+
     @Test
-    void shouldTransferMoneyBetweenOwnCardsV2() {
-        open("http://localhost:9999");
-        var loginPage = new LoginPageV2();
-//    var loginPage = open("http://localhost:9999", LoginPageV2.class);
-        var authInfo = DataHelper.getAuthInfo();
-        var verificationPage = loginPage.validLogin(authInfo);
-        var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
-        verificationPage.validVerify(verificationCode);
+    void shouldTransferMoneyBetweenOwnCardsV4() {
+        int firstCardStartBalance = DashboardPage.getCardBalance(0);
+        int secondCardStartBalance = DashboardPage.getCardBalance(1);
+        int transferAmount = 394;
+        DashboardPage.changeFirstCardBalanse(Integer.toString(transferAmount));
+        assertEquals(firstCardStartBalance + transferAmount, DashboardPage.getCardBalance(0));
+        assertEquals(secondCardStartBalance - transferAmount, DashboardPage.getCardBalance(1));
     }
 
     @Test
-    void shouldTransferMoneyBetweenOwnCardsV3() {
-        var loginPage = open("http://localhost:9999", LoginPageV3.class);
-        var authInfo = DataHelper.getAuthInfo();
-        var verificationPage = loginPage.validLogin(authInfo);
-        var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
-        verificationPage.validVerify(verificationCode);
+    void shouldTransferMoneyBetweenOwnCardsV5() {
+        int firstCardStartBalance = DashboardPage.getCardBalance(0);
+        int secondCardStartBalance = DashboardPage.getCardBalance(1);
+        int transferAmount = 748;
+        DashboardPage.changeSecondCardBalanse(Integer.toString(transferAmount));
+        assertEquals(firstCardStartBalance - transferAmount, DashboardPage.getCardBalance(0));
+        assertEquals(secondCardStartBalance + transferAmount, DashboardPage.getCardBalance(1));
     }
 }
